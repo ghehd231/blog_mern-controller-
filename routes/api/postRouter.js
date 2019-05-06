b572ac2d140790d6f7d77e9aed2c279fc8df76b4
ghehd231 <ghehd231@naver.com>
2019-05-06 17:49:27 +0900

    Add create or update profile to profileController

--- routes/api/postRouter.js
+++ routes/api/postRouter.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+/** controller */
 const postController = require('../../controllers/postController');
-
-/** Use passport */
-const passport = require('passport');
-const authCheck = passport.authenticate('jwt', {session: false});
-
 
 /**
  * @route   GET api/post/test
@@ -15,13 +11,5 @@ const authCheck = passport.authenticate('jwt', {session: false});
  */
 router.route('/test')
     .post(postController.test);
-
-/**
- * @route   GET api/profile
- * @desc    Get current users profile
- * @access  Private
- */
-router.route('/').get(authCheck, profileController.home);
-
 
 module.exports = router;
--- routes/api/profileRouter.js
+++ routes/api/profileRouter.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+/** controller */
 const profileController = require('../../controllers/profileController');
 
+/** Use passport */
+const passport = require('passport');
+const authCheck = passport.authenticate('jwt', {session: false});
+
+
+/**
+ * @route   GET api/profile/test
+ * @desc    Tests profile route
+ * @access  Public
+ */
 router.route('/test')
     .post(profileController.test);
+
+/**
+ * @route   GET api/profile
+ * @desc    Get current users profile
+ * @access  Private
+ */
+router.route('/').get(authCheck, profileController.getProfile);
+
+/**
+ * @route   POST api/profile
+ * @desc    Create or edit user profile
+ * @access  Private
+ */
+router.route('/').post(authCheck, profileController.postProfile);
 
 module.exports = router;

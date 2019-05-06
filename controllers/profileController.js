554213682c30eee0c9a011be1034d891a0f39d7e
ghehd231 <ghehd231@naver.com>
2019-05-06 17:31:09 +0900

    Add profile Schema and get profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,5 +1,31 @@
+const profileModel = require('../models/profileModel');
+const userModel = require('../models/userModel');
+
 module.exports = {
+
+    /**
+@@ -7,5 +10,24 @@ module.exports = {
+     */
     test: async (req, res) => {
-        res.status(200).json({message: "profile works"})
+        res.status(200).json({message: "Profile Works!"});
+    },
+
+    /**
+     * @controller  GET api/profile
+     * @desc        Get current users profile
+     * @access      Private
+     */
+    home: async (req, res) => {
+        const errors = {};
+
+        profileModel.findOne({user: req.user.id})
+            .then(profile => {
+                if(!profile){
+                    errors.noProfile = 'No profile for this user.'
+                    return res.status(400).json(errors);
+                }
+                res.json(profile);
+            })
+            .catch(err => res.status(404).json(err));
     }
-}
+} 
--- routes/api/postRouter.js
+++ routes/api/postRouter.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const postController = require('../../controllers/postController');
 
+/** Use passport */
+const passport = require('passport');
+const authCheck = passport.authenticate('jwt', {session: false});
+
+
 /**
  * @route   GET api/post/test
  * @desc    Tests post route
@@ -10,5 +15,13 @@ const postController = require('../../controllers/postController');
  */
 router.route('/test')
     .post(postController.test);
+
+/**
+ * @route   GET api/profile
+ * @desc    Get current users profile
+ * @access  Private
+ */
+router.route('/').get(authCheck, profileController.home);
+
 
 module.exports = router;
